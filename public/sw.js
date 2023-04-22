7a2bb93ec9d67f41dc0aa1de2e11f96c027f619c
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2023-04-22 21:34:47 +1000

    change

--- public/sw.js
+++ public/sw.js
@@ -1,48 +1,55 @@
-// Define a cache name
-const CACHE_NAME = 'my-site-cache-v1'
+const CACHE_NAME = 'my-pwa-cache'
+const urlsToCache = ['/']
 
-// Define the files to cache
-const urlsToCache = ['/', '/index.html', '/css/style.css', '/js/app.js', '/images/logo.png']
-
-// Install the service worker
 self.addEventListener('install', (event) => {
+  // Perform the install steps
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('Cache opened')
+      console.log('Opened cache')
       return cache.addAll(urlsToCache)
     })
   )
 })
 
-// Fetch resources from cache or network
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
+      // Cache hit - return response
       if (response) {
-        // Found in cache, return cached response
         return response
       }
-      // Not found in cache, fetch from network
-      return fetch(event.request).then((response) => {
-        // Cache the fetched resource
-        const cacheCopy = response.clone()
+
+      // Clone the request to make a fetch request
+      const fetchRequest = event.request.clone()
+
+      return fetch(fetchRequest).then((response) => {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response
+        }
+
+        // Clone the response to store in the cache
+        const responseToCache = response.clone()
+
         caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, cacheCopy)
+          cache.put(event.request, responseToCache)
         })
+
         return response
       })
     })
   )
 })
 
-// Clean up old caches
 self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME]
+
   event.waitUntil(
-    caches.keys().then((keyList) => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key)
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName)
           }
         })
       )
