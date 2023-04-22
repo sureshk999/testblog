60022f30c38980f2709564fa7421cef2c1238482
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2023-04-22 20:33:40 +1000

    add more PWA stuff

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,51 @@
-// Import the Workbox libraries
-import { precacheAndRoute } from 'workbox-precaching'
-import { registerRoute } from 'workbox-routing'
-import { NetworkFirst } from 'workbox-strategies'
+// Define a cache name
+const CACHE_NAME = 'my-site-cache-v1'
 
-// Precache all static assets
-precacheAndRoute(self.__WB_MANIFEST)
+// Define the files to cache
+const urlsToCache = ['/', '/index.html', '/css/style.css', '/js/app.js', '/images/logo.png']
 
-// Cache API requests with the NetworkFirst strategy
-registerRoute(({ url }) => url.origin === 'https://api.example.com', new NetworkFirst())
+// Install the service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Cache opened')
+      return cache.addAll(urlsToCache)
+    })
+  )
+})
+
+// Fetch resources from cache or network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        // Found in cache, return cached response
+        return response
+      }
+      // Not found in cache, fetch from network
+      return fetch(event.request).then((response) => {
+        // Cache the fetched resource
+        const cacheCopy = response.clone()
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, cacheCopy)
+        })
+        return response
+      })
+    })
+  )
+})
+
+// Clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key)
+          }
+        })
+      )
+    })
+  )
+})
