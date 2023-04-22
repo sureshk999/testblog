57376460556723b3d22b95ce836359bb76c8fafc
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2023-04-22 21:09:25 +1000

    add service worker to app

--- pages/_app.js
+++ pages/_app.js
@@ -11,12 +11,19 @@ import siteMetadata from '@/data/siteMetadata'
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
+
 import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { AppProps } from 'next/app'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
 
-export default function App({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  const site = 'https://drsuresh.com.au'
+  const canonicalURL = site + useRouter().asPath
+
   useEffect(() => {
     if ('serviceWorker' in navigator) {
       window.addEventListener('load', function () {
