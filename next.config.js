971fb8653c94babd86e11f169b69ca9e3e5cfd8c
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2023-04-22 20:23:57 +1000

    add more PWA stuff

--- next.config.js
+++ next.config.js
@@ -2,7 +2,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
+/** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+module.exports = {
+  swcMinify: true,
+}
 
 module.exports = withPWA({
   pwa: {
